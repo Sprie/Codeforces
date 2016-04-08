2ca7d06c390242d5f1a100b707a877bb3a96ca64
Sprie <zhouaxic@gmail.com>
2016-04-08 20:30:54 +0800

    1，修复启动界面的单词错误 2，添加了问题详情的图片显示并适配了不同分辨率的屏幕，但未存储本地，每次打开都需要联网获取图片

--- app/src/main/java/com/axic/codeforces/fragment/Home.java
+++ app/src/main/java/com/axic/codeforces/fragment/Home.java
@@ -1,28 +1,58 @@
 package com.axic.codeforces.fragment;
 
 import android.app.Fragment;
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+import com.android.volley.toolbox.Volley;
 import com.axic.codeforces.R;
 
 /**
  * Created by 59786 on 2016/3/27.
  */
-public class Home extends Fragment{
+public class Home extends Fragment {
     private View view;
+    private RequestQueue mQueue;
+    private ImageView imageView;
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.homepage, container, false);
 //        view = getPersistentView(inflater, container, savedInstanceState);
-
-
+        imageView = (ImageView) view.findViewById(R.id.imageView);
+        getImageFromNet();
         return view;
     }
 
+    //test
+    public void getImageFromNet() {
+        mQueue = Volley.newRequestQueue(getActivity());
+
+        ImageRequest imageRequest = new ImageRequest(
+                //url
+                "http://pic.to8to.com/attch/day_160218/20160218_a9c8ab4599980f55577bp7at2oEwM7s9.png",
+                new Response.Listener<Bitmap>() {
+                    @Override
+                    public void onResponse(Bitmap response) {
+                        imageView.setImageBitmap(response);
+                    }
+                }, 0, 0, Bitmap.Config.RGB_565, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                imageView.setImageResource(R.drawable.back);
+            }
+        }
+        );
+        mQueue.add(imageRequest);
+    }
 
 //    private View getPersistentView(LayoutInflater inflater, ViewGroup container,
 //                                   Bundle savedInstanceState) {
--- app/src/main/java/com/axic/codeforces/fragment/ProblemsDetails.java
+++ app/src/main/java/com/axic/codeforces/fragment/ProblemsDetails.java
@@ -1,11 +1,12 @@
 package com.axic.codeforces.fragment;
 
 import android.app.Fragment;
+import android.graphics.drawable.Drawable;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Message;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.text.Html;
+import android.text.Spanned;
+import android.util.DisplayMetrics;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -17,7 +18,9 @@ import com.axic.codeforces.R;
 import com.axic.codeforces.database.ProblemsDBManager;
 import com.axic.codeforces.method.CheckNet;
 import com.axic.codeforces.method.GetProblemInfoFromHtml;
-import com.axic.codeforces.method.MyTextView;
+
+import java.io.InputStream;
+import java.net.URL;
 
 
 /**
@@ -36,7 +39,10 @@ public class ProblemsDetails extends Fragment implements SwipeRefreshLayout.OnRe
     private ProblemsDBManager db;
     private GetProblemInfoFromHtml getProblemInfoFromHtml;
     private String html = "error";
+    private Spanned details;
     boolean status = true;
+    private float density;
+    private float scaledDensity;
 
 
 //    private Handler handler = new Handler() {
@@ -64,13 +70,27 @@ public class ProblemsDetails extends Fragment implements SwipeRefreshLayout.OnRe
         mSwipe.setOnRefreshListener(this);
         checkNet = new CheckNet(getActivity());
 
+        //test
+        DisplayMetrics metric = new DisplayMetrics();
+        getActivity().getWindowManager().getDefaultDisplay().getMetrics(metric);
+//        int width = metric.widthPixels;  // 屏幕宽度（像素）
+//        int height = metric.heightPixels;  // 屏幕高度（像素）
+        density = metric.density;  // 屏幕密度（0.75 / 1.0 / 1.5）
+//        int densityDpi = metric.densityDpi;  // 屏幕密度DPI（120 / 160 / 240）
+        scaledDensity = metric.scaledDensity;
+//        Log.d("dpi", width + "  " + height + "  " + density + "  " + densityDpi);
+        Log.d("density", density + "");
+//        Toast.makeText(getActivity(),
+//                width + "  "+height+"  "+density+"  "+densityDpi,
+//                Toast.LENGTH_LONG).show();
+
         tv.setText("Loading...");
 
         tv.setOnLongClickListener(new View.OnLongClickListener() {
             @Override
             public boolean onLongClick(View v) {
 
-                Log.d("onLongClick","true");
+                Log.d("onLongClick", "true");
                 return false;
             }
         });
@@ -114,8 +134,67 @@ public class ProblemsDetails extends Fragment implements SwipeRefreshLayout.OnRe
 
     private void getDataFromDB() {
 
-        tv.setText(Html.fromHtml(db.getDetails(contestId, index)));
-        Log.d("details", "getFromDB");
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+
+
+                //使用textview显示获取的html中的文字和图片
+                details = Html.fromHtml(db.getDetails(contestId, index), new Html.ImageGetter() {
+                    @Override
+                    public Drawable getDrawable(String source) {
+                        InputStream is = null;
+                        try {
+//                            DisplayMetrics dm = new DisplayMetrics();
+//                            Log.d("dm.density", dm.density + " , "+dm.densityDpi);
+                            Log.d("getImage", "success");
+                            is = (InputStream) new URL(source).getContent();
+
+                            Drawable d = Drawable.createFromStream(is, "src");
+                            //屏幕适配
+                            float height = 16 * scaledDensity + 0.5f;
+                            Log.d("hhhh", height + "");
+                            if (d.getIntrinsicHeight() < height) {
+                                d.setBounds(0, 0, (int) (height / d.getIntrinsicHeight() * d.getIntrinsicWidth()),//因为density是float，而这儿的参数需要int
+                                        (int) height);//所以强制装换为int
+                            } else {
+                                Log.d("density", density + "");
+                                if (density >= 3.0) {
+                                    d.setBounds(0, 0, (int) (d.getIntrinsicWidth() * density * 2),//以为density是float，而这儿的参数需要int
+                                            (int) (d.getIntrinsicHeight() * density * 2));//所以强制装换为int
+                                    Log.d("*2", ">3");
+                                } else {
+                                    d.setBounds(0, 0, (int) (d.getIntrinsicWidth() * density),//以为density是float，而这儿的参数需要int
+                                            (int) (d.getIntrinsicHeight() * density));//所以强制装换为int
+                                }
+                            }
+
+                            Log.d("height width", d.getIntrinsicHeight() + "  " + d.getIntrinsicWidth() + " , ");
+                            Log.d("height ", d.getIntrinsicHeight() + "  " + (int) (d.getIntrinsicHeight() * density) + " , "
+                                    + (d.getIntrinsicHeight() * (int) density));
+                            Log.d("width", d.getIntrinsicWidth() + "  " + (int) (d.getIntrinsicWidth() * density) + " , "
+                                    + (d.getIntrinsicWidth() * (int) density));
+
+//                            Toast.makeText(getActivity(),dm.density + " , "+dm.densityDpi,Toast.LENGTH_LONG).show();
+                            is.close();
+                            return d;
+                        } catch (Exception e) {
+                            Log.d("getImage", "failed");
+                            return null;
+                        }
+                    }
+                }, null);
+                getActivity().runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        tv.setText(details);
+                        Log.d("details", "getFromDB");
+                    }
+                });
+            }
+
+        }).start();
+
     }
 
     private void getDataFromNet() {
@@ -128,17 +207,53 @@ public class ProblemsDetails extends Fragment implements SwipeRefreshLayout.OnRe
                 public void run() {
                     Log.d("comingNewThread", "coming");
                     status = getProblemInfoFromHtml.run();
+
                     if (status) {
                         Log.d("status", status + "...");
                         html = getProblemInfoFromHtml.getHtml();
                         //test
 //                        Log.d("header",getProblemInfoFromHtml.getHeaderData());
 
+                        //使用textview显示获取的html中的文字和图片
+                        details = Html.fromHtml(html, new Html.ImageGetter() {
+                            @Override
+                            public Drawable getDrawable(String source) {
+                                InputStream is = null;
+                                try {
+                                    Log.d("getImage", "success");
+                                    is = (InputStream) new URL(source).getContent();
+                                    Log.d("data", is.toString());
+                                    Drawable d = Drawable.createFromStream(is, "src");
+                                    //屏幕适配
+                                    float height = 16 * scaledDensity + 0.5f;
+                                    Log.d("hhhh", height + "");
+                                    if (d.getIntrinsicHeight() < height) {
+                                        d.setBounds(0, 0, (int) (height / d.getIntrinsicHeight() * d.getIntrinsicWidth()),//因为density是float，而这儿的参数需要int
+                                                (int) height);//所以强制装换为int
+                                    } else {
+                                        Log.d("density", density + "");
+                                        if (density >= 3.0) {
+                                            d.setBounds(0, 0, (int) (d.getIntrinsicWidth() * density * 2),//以为density是float，而这儿的参数需要int
+                                                    (int) (d.getIntrinsicHeight() * density * 2));//所以强制装换为int
+                                            Log.d("*2", ">3");
+                                        } else {
+                                            d.setBounds(0, 0, (int) (d.getIntrinsicWidth() * density),//以为density是float，而这儿的参数需要int
+                                                    (int) (d.getIntrinsicHeight() * density));//所以强制装换为int
+                                        }
+                                    }
+                                    is.close();
+                                    return d;
+                                } catch (Exception e) {
+                                    Log.d("getImage", "failed");
+                                    return null;
+                                }
+                            }
+                        }, null);
 
                         getActivity().runOnUiThread(new Runnable() {
                             @Override
                             public void run() {
-                                tv.setText(Html.fromHtml(html));
+                                tv.setText(details);
                                 Log.d("In ", "Ui");
                             }
                         });
