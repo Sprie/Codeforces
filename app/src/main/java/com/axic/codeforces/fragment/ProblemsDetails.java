e96c8d58efee46341a2d1ad55c1e247ec0dd6c0c
Sprie <zhouaxic@gmail.com>
2016-04-10 16:59:18 +0800

    1,完善Details的图片显示 2,Details内容可复制 3,启动页面使用压缩图片，进一步减少apk大小

--- app/src/main/java/com/axic/codeforces/fragment/ProblemsDetails.java
+++ app/src/main/java/com/axic/codeforces/fragment/ProblemsDetails.java
@@ -1,6 +1,7 @@
 package com.axic.codeforces.fragment;
 
 import android.app.Fragment;
+import android.content.Context;
 import android.graphics.drawable.Drawable;
 import android.os.Bundle;
 import android.support.v4.widget.SwipeRefreshLayout;
@@ -30,7 +31,7 @@ public class ProblemsDetails extends Fragment implements SwipeRefreshLayout.OnRe
 
     private View view;
     private TextView tv;
-    private String problemUrl = "http://codeforces.com/problemset/problem/";
+    private String problemUrl;
     private String contestId;
     private String index;
     private String Url;
@@ -45,6 +46,9 @@ public class ProblemsDetails extends Fragment implements SwipeRefreshLayout.OnRe
     private float scaledDensity;
     private Thread mThread;
     private boolean mThreadState = true;
+    int screenWidth;
+    int screenHeight;
+    Context mContext;
 
 
 //    private Handler handler = new Handler() {
@@ -63,28 +67,29 @@ public class ProblemsDetails extends Fragment implements SwipeRefreshLayout.OnRe
 
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        db = new ProblemsDBManager(getActivity(), 1);
+        mContext = getActivity();
+        db = new ProblemsDBManager(mContext, 1);
         view = inflater.inflate(R.layout.problemsdetails, container, false);
         tv = (TextView) view.findViewById(R.id.details);
 
         mSwipe = (SwipeRefreshLayout) view.findViewById(R.id.details_swipe);
         mSwipe.setColorSchemeResources(android.R.color.black);
         mSwipe.setOnRefreshListener(this);
-        checkNet = new CheckNet(getActivity());
+        checkNet = new CheckNet(mContext);
 
         //test
         DisplayMetrics metric = new DisplayMetrics();
         getActivity().getWindowManager().getDefaultDisplay().getMetrics(metric);
-//        int width = metric.widthPixels;  // 屏幕宽度（像素）
-//        int height = metric.heightPixels;  // 屏幕高度（像素）
+        screenWidth = metric.widthPixels;  // 屏幕宽度（像素）
+        screenHeight = metric.heightPixels;  // 屏幕高度（像素）
         density = metric.density;  // 屏幕密度（0.75 / 1.0 / 1.5）
-//        int densityDpi = metric.densityDpi;  // 屏幕密度DPI（120 / 160 / 240）
+        int densityDpi = metric.densityDpi;  // 屏幕密度DPI（120 / 160 / 240）
         scaledDensity = metric.scaledDensity;
 //        Log.d("dpi", width + "  " + height + "  " + density + "  " + densityDpi);
         Log.d("density", density + "");
-//        Toast.makeText(getActivity(),
-//                width + "  "+height+"  "+density+"  "+densityDpi,
-//                Toast.LENGTH_LONG).show();
+        Toast.makeText(mContext,
+                screenWidth + "  " + screenHeight + "  " + density + "  " + densityDpi,
+                Toast.LENGTH_LONG).show();
 
         tv.setText(R.string.Loading);
 
@@ -96,8 +101,11 @@ public class ProblemsDetails extends Fragment implements SwipeRefreshLayout.OnRe
                 return false;
             }
         });
-
         Bundle bundle = getArguments();
+        //以下两种url的方法都可以用的，但第二种获取的比较丰富点-----------
+        // 但是第一种方法连接恒快，所以用第一种连接
+
+        problemUrl = "http://codeforces.com/problemset/problem/";
         if (bundle != null) {
             contestId = bundle.getString("contestId");
             index = bundle.getString("index");
@@ -108,6 +116,16 @@ public class ProblemsDetails extends Fragment implements SwipeRefreshLayout.OnRe
         } else {
             tv.setText("error");
         }
+//        problemUrl = "http://codeforces.com/contest/";
+//        if (bundle != null) {
+//            contestId = bundle.getString("contestId");
+//            index = bundle.getString("index");
+//            Url = problemUrl + contestId + "/problem/" + index;
+//            Log.d("url", Url);
+//        } else {
+//            tv.setText("error");
+//        }
+
         getProblemInfoFromHtml = new GetProblemInfoFromHtml(Url);
 
         getDataFromNet();
@@ -127,7 +145,7 @@ public class ProblemsDetails extends Fragment implements SwipeRefreshLayout.OnRe
             getDataFromNet();
             mSwipe.setRefreshing(false);
         } else {
-            Toast.makeText(getActivity(), "当前无网络", Toast.LENGTH_LONG).show();
+            Toast.makeText(mContext, "当前无网络", Toast.LENGTH_LONG).show();
 
             getDataFromDB();
             mSwipe.setRefreshing(false);
@@ -154,30 +172,8 @@ public class ProblemsDetails extends Fragment implements SwipeRefreshLayout.OnRe
 
                             Drawable d = Drawable.createFromStream(is, "src");
                             //屏幕适配
-                            float height = 16 * scaledDensity + 0.5f;
-                            Log.d("hhhh", height + "");
-                            if (d.getIntrinsicHeight() < height) {
-                                d.setBounds(0, 0, (int) (height / d.getIntrinsicHeight() * d.getIntrinsicWidth()),//因为density是float，而这儿的参数需要int
-                                        (int) height);//所以强制装换为int
-                            } else {
-                                Log.d("density", density + "");
-                                if (density >= 3.0) {
-                                    d.setBounds(0, 0, (int) (d.getIntrinsicWidth() * density * 2),//以为density是float，而这儿的参数需要int
-                                            (int) (d.getIntrinsicHeight() * density * 2));//所以强制装换为int
-                                    Log.d("*2", ">3");
-                                } else {
-                                    d.setBounds(0, 0, (int) (d.getIntrinsicWidth() * density),//以为density是float，而这儿的参数需要int
-                                            (int) (d.getIntrinsicHeight() * density));//所以强制装换为int
-                                }
-                            }
+                            d = screenAdaptation(d);
 
-                            Log.d("height width", d.getIntrinsicHeight() + "  " + d.getIntrinsicWidth() + " , ");
-                            Log.d("height ", d.getIntrinsicHeight() + "  " + (int) (d.getIntrinsicHeight() * density) + " , "
-                                    + (d.getIntrinsicHeight() * (int) density));
-                            Log.d("width", d.getIntrinsicWidth() + "  " + (int) (d.getIntrinsicWidth() * density) + " , "
-                                    + (d.getIntrinsicWidth() * (int) density));
-
-//                            Toast.makeText(getActivity(),dm.density + " , "+dm.densityDpi,Toast.LENGTH_LONG).show();
                             is.close();
                             return d;
                         } catch (Exception e) {
@@ -227,22 +223,7 @@ public class ProblemsDetails extends Fragment implements SwipeRefreshLayout.OnRe
                                     Log.d("data", is.toString());
                                     Drawable d = Drawable.createFromStream(is, "src");
                                     //屏幕适配
-                                    float height = 16 * scaledDensity + 0.5f;
-                                    Log.d("hhhh", height + "");
-                                    if (d.getIntrinsicHeight() < height) {
-                                        d.setBounds(0, 0, (int) (height / d.getIntrinsicHeight() * d.getIntrinsicWidth()),//因为density是float，而这儿的参数需要int
-                                                (int) height);//所以强制装换为int
-                                    } else {
-                                        Log.d("density", density + "");
-                                        if (density >= 3.0) {
-                                            d.setBounds(0, 0, (int) (d.getIntrinsicWidth() * density * 2),//以为density是float，而这儿的参数需要int
-                                                    (int) (d.getIntrinsicHeight() * density * 2));//所以强制装换为int
-                                            Log.d("*2", ">3");
-                                        } else {
-                                            d.setBounds(0, 0, (int) (d.getIntrinsicWidth() * density),//以为density是float，而这儿的参数需要int
-                                                    (int) (d.getIntrinsicHeight() * density));//所以强制装换为int
-                                        }
-                                    }
+                                    d = screenAdaptation(d);
                                     is.close();
                                     return d;
                                 } catch (Exception e) {
@@ -268,7 +249,6 @@ public class ProblemsDetails extends Fragment implements SwipeRefreshLayout.OnRe
                         }
 
 
-
                     } else {
                         Log.d("status", status + "--");
                         //若未退出该页面
@@ -279,7 +259,7 @@ public class ProblemsDetails extends Fragment implements SwipeRefreshLayout.OnRe
                                 public void run() {
                                     tv.setText("获取失败，请稍后重试");
 
-//                                Toast.makeText(getActivity(),"服务器正在开小差~请稍候重试 >-<",Toast.LENGTH_LONG).show();
+//                                Toast.makeText(mContext,"服务器正在开小差~请稍候重试 >-<",Toast.LENGTH_LONG).show();
                                     Log.d("In ", "Ui");
                                 }
                             });
@@ -297,6 +277,57 @@ public class ProblemsDetails extends Fragment implements SwipeRefreshLayout.OnRe
             getDataFromDB();
         }
 
+    }
+
+    //屏幕适配
+    private Drawable screenAdaptation(Drawable d) {
+        float height;
+        if (density >= 3) {
+            height = 10 * scaledDensity + 0.5f;
+        } else {
+            height = 16 * scaledDensity + 0.5f;
+        }
+        Log.d("hhhh", height + " /// " + scaledDensity);
+        if (d.getIntrinsicHeight() < height) {
+            d.setBounds(0, 0, (int) (height*2 / d.getIntrinsicHeight() * d.getIntrinsicWidth()),//因为density是float，而这儿的参数需要int
+                    (int) height*2);//所以强制装换为int
+        } else {
+            Log.d("density", density + "");
+            if (density >= 3.0) {
+                int width, h;
+                if ((int) (d.getIntrinsicWidth() * density * 3) > screenWidth-20) {
+                    Log.d("small screen", "fill width");
+                    width = screenWidth-20;
+                    h = width * d.getIntrinsicHeight()/ d.getIntrinsicWidth() ;
+                    d.setBounds(0, 0, width, h);//所以强制装换为int
+                    Log.d("changedHW", width + " / / " + h);
+                } else {
+                    d.setBounds(0, 0, (int) (d.getIntrinsicWidth() * density * 3),//以为density是float，而这儿的参数需要int
+                            (int) (d.getIntrinsicHeight() * density * 3));//所以强制装换为int
+                    Log.d("*2", ">3");
+                }
+            } else {
+                int width, h;
+                if ((int) (d.getIntrinsicWidth() * density) > screenWidth) {
+                    Log.d("small screen", "fill width");
+                    width = screenWidth;
+                    h = d.getIntrinsicHeight() / d.getIntrinsicWidth() * width;
+                    d.setBounds(0, 0, width, h);//所以强制装换为int
+                } else {
+
+                    d.setBounds(0, 0, (int) (d.getIntrinsicWidth() * density),//以为density是float，而这儿的参数需要int
+                            (int) (d.getIntrinsicHeight() * density));//所以强制装换为int
+                }
+            }
+        }
+        Log.d("height width", d.getIntrinsicHeight() + "  " + d.getIntrinsicWidth() + " , ");
+        Log.d("height ", d.getIntrinsicHeight() + "  " + (int) (d.getIntrinsicHeight() * density) + " , "
+                + (d.getIntrinsicHeight() * (int) density));
+        Log.d("width", d.getIntrinsicWidth() + "  " + (int) (d.getIntrinsicWidth() * density) + " , "
+                + (d.getIntrinsicWidth() * (int) density));
+
+//                            Toast.makeText(mContext,dm.density + " , "+dm.densityDpi,Toast.LENGTH_LONG).show();
+        return d;
     }
 
 
